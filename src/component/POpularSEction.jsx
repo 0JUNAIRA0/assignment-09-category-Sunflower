0d341ac0ce150770d243d4f14a11a2caefcc10c7
Junaira Islam <junairaislam1234@gmail.com>
2025-11-23 11:16:49 +0600

    added cards and json data

---
+++ src/component/POpularSEction.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+const POpularSEction = () => {
+
+    const[services, setServices]= useState([]);
+
+    useEffect(() => {
+        fetch('jasondata.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+        .catch(err => console.log(err))
+    }, [] )
+
+    console.log(services);
+       
+    return (
+        <div className='mt-8 px-[145px]'>
+            <div>
+                <h3 className='font-bold text-3xl text-center'>Popular Winter Care Services</h3>
+            </div>
+
+          <div className='grid grid-cols-3 mt-12'>
+             {
+            services.map(service => 
+                <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img src={service?.image} alt="" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{service?.serviceName} </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+            )
+           }
+          </div>
+
+
+        </div>
+    );
+};
+
+export default POpularSEction;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Slider from '../component/Slider';
+import POpularSEction from '../component/POpularSEction';
+
+const Home = () => {
+    return (
+        <div>
+            <Slider></Slider>
+            <POpularSEction></POpularSEction>
+        </div>
+    );
+};
+
+export default Home;
